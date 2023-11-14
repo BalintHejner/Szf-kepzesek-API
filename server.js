71498d3d8b61d050d709c74c22a90dd3eb70032d
BalintHejner <balinthejner204@gmail.com>
2023-11-14 09:38:55 +0100

    :art: End of class commit

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const trainings = require("./routes/trainings");
+
 require("dotenv").config(); // A .env fájlt olvassa
 const app = express();
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const morgan = require("morgan");
 
-app.get('/', (req, res) => {
-     res.status(400).json({ success: false})
-    })
-    
-const trainings = require('./routes/trainings')
-app.use('/api/trainings', trainings)
-    
+app.use(morgan('dev'));
+
+app.use("/api/trainings", trainings);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
+
+app.get("/", (req, res) => {
+  res.status(400).json({ success: false });
+});

241d44ba0ef4fe6edffe5bb495dd7e997c73dec0
BalintHejner <balinthejner204@gmail.com>
2023-11-21 09:01:30 +0100

    :monocle_face: Adding data to the Database

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -2,7 +2,9 @@
 // @route  GET /api/trainings
 // @access Public
 exports.getTrainings = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all trainings", hello: req.hello });
+  res
+    .status(200)
+    .json({ success: true, msg: "Show all trainings", hello: req.hello });
 }; // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
@@ -12,21 +14,34 @@ exports.getTraining = (req, res, next) => {
 // @route  POST /api/trainings
 // @access Private
 exports.createTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new training" });
+  exports.createTraining = async (req, res, next) => {
+    try {
+      const training = await Training.create(req.body);
+      res.status(201).json({ success: true, data: training });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  };
 }; // @desc   Update training
 // @route  PUT /api/trainings/:id
 // @access Private
 exports.updateTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Update training ${req.params.id}` });
+  res
+    .status(200)
+    .json({ success: true, msg: `Update training ${req.params.id}` });
 }; // @desc   Delete training
 // @route  DELETE /api/trainings/:id
 // @access Private
 exports.deleteTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Delete training ${req.params.id}` });
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete training ${req.params.id}` });
 };
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
 exports.getTrainings = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all trainings", hello: req.hello });
+  res
+    .status(200)
+    .json({ success: true, msg: "Show all trainings", hello: req.hello });
 };
